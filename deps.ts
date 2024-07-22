bfdd275180e1188821b60ee6708741b10dde1c72
Omar Khan <o.khandxb@gmail.com>
2024-07-21 22:08:21 -0400

    db transfer completed

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export * as csv from "jsr:@vslinko/csv";
 export * as mod from "jsr:@libs/xml/";
+export * as sqlite from "https://deno.land/x/sqlite@v3.8/mod.ts";
--- main.ts
+++ main.ts
@@ -1,14 +1,40 @@
-import { csv } from "./deps.ts";
-import { mod } from "./deps.ts"
+import { xml_text } from "jsr:@libs/xml/parse";
+import { csv, mod, sqlite } from "./deps.ts";
+
+type returnedId = number[]
+
+type trkPt = {
+  "@lat": string,
+  "@lon": string,
+  "ele": string,
+  "time": string
+}
+
+interface MyXML extends xml_text {
+  trk: {
+    trkseg:{
+      trkpt:trkPt[]
+    }
+  }; 
+}
+
+const db = new sqlite.DB("./strava.db");
 
 const f = await Deno.open("./activities.csv");
-
+//1468519748
   for await (const obj of csv.readCSVObjects(f)) {
-    if(Date.parse(obj['Activity Date']).toString() > '1462752008' && Date.parse(obj['Activity Date']).toString() < '1462838782'){
-      console.log(mod.parse(await Deno.readTextFile(obj['Filename']))["~children"][0].trk)
+    if(Date.parse(obj['Activity Date']).toString() > '1462752008' && Date.parse(obj['Activity Date']).toString() < '1468519748'){
+      const id: sqlite.Row = db.query("INSERT INTO rides (name) VALUES (?) RETURNING ID", [obj['Activity Name']]);
+      const rideId = id[0] as returnedId
+      const customXml: MyXML = mod.parse(await Deno.readTextFile(obj['Filename']))["~children"][0] as MyXML
+      for (const point of customXml["trk"].trkseg.trkpt){
+        db.query("INSERT INTO coordinates (latitude, longitude, elevation, time, ride_id) VALUES (?, ?, ?, ?, ?)", [point["@lat"], point["@lon"], point.ele, point.time, rideId[0]]);
+      }
       console.log(obj['Activity Name'] + ' - ' + obj['Filename']);
     }
   }
 
-
+db.close();
 f.close();
+
+Deno.exit(0)
