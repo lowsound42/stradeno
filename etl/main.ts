3bf5eaba73f97205f9cbd6e5adfdf1b61f8dd9d3
Omar Khan <o.khandxb@gmail.com>
2024-08-03 17:35:58 -0400

    adding vue app to the mix

--- etl/main.ts
+++ etl/main.ts
@@ -24,11 +24,12 @@ const f = await Deno.open("./activities.csv");
 //1468519748
   for await (const obj of csv.readCSVObjects(f)) {
     if(Date.parse(obj['Activity Date']).toString() > '1462752008' && Date.parse(obj['Activity Date']).toString() < '1468519748'){
-      const id: sqlite.Row = db.query("INSERT INTO rides (name) VALUES (?) RETURNING ID", [obj['Activity Name']]);
-      const rideId = id[0] as returnedId
+      // const id: sqlite.Row = db.query("INSERT INTO rides (name) VALUES (?) RETURNING ID", [obj['Activity Name']]);
+      // const rideId = id[0] as returnedId
       const customXml: MyXML = mod.parse(await Deno.readTextFile(obj['Filename']))["~children"][0] as MyXML
       for (const point of customXml["trk"].trkseg.trkpt){
-        db.query("INSERT INTO coordinates (latitude, longitude, elevation, time, ride_id) VALUES (?, ?, ?, ?, ?)", [point["@lat"], point["@lon"], point.ele, point.time, rideId[0]]);
+        console.log(point['@lat'], point['@lon'])
+        // db.query("INSERT INTO coordinates (latitude, longitude, elevation, time, ride_id) VALUES (?, ?, ?, ?, ?)", [point["@lat"], point["@lon"], point.ele, point.time, rideId[0]]);
       }
       console.log(obj['Activity Name'] + ' - ' + obj['Filename']);
     }
